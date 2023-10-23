efb46ceab0059fc59677480df1fb9a7c8079f9f6
Aymeric Gatat <gatataymeric@hotmail.fr>
2023-10-23 16:21:38 +0200

    Premier commit et mis en place du fetch pour la page d'accueil avec modification du template

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,46 +1,47 @@
-    async function getPhotographers() {
-        // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
-        // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        let photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
-        // et bien retourner le tableau photographers seulement une fois récupéré
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+// Cette fonction asynchrone est responsable de la récupération des données des photographes depuis un fichier JSON distant.
+async function getPhotographers() {
+  try {
+    // Utilise la fonction Fetch pour obtenir les données du fichier JSON
+    const response = await fetch("../../data/photographers.json");
+
+    // Vérifie si la requête a réussi (statut HTTP 200)
+    if (!response.ok) {
+      throw new Error("La requête a échoué");
     }
 
-    async function displayData(photographers) {
-        const photographersSection = document.querySelector(".photographer_section");
+    // Analyse les données JSON de la réponse
+    const data = await response.json();
+    console.log(data.photographers);
+    // Retourne les photographes sous forme d'objet
+    return data;
+  } catch (error) {
+    // Gère les erreurs en cas d'échec de la requête
+    console.error(error);
+  }
+}
 
-        photographers.forEach((photographer) => {
-            const photographerModel = photographerTemplate(photographer);
-            const userCardDOM = photographerModel.getUserCardDOM();
-            photographersSection.appendChild(userCardDOM);
-        });
-    }
+// Cette fonction asynchrone affiche les données des photographes sur la page web
+async function displayData(photographers) {
+  const photographersSection = document.querySelector(".photographer_section");
 
-    async function init() {
-        // Récupère les datas des photographes
-        const { photographers } = await getPhotographers();
-        displayData(photographers);
-    }
-    
-    init();
-    
+  // Itère sur chaque photographe et crée un modèle de carte pour l'afficher sur la page
+  photographers.forEach((photographer) => {
+    const photographerModel = photographerTemplate(photographer);
+    const userCardDOM = photographerModel.getUserCardDOM();
+
+    // Ajoute la carte de l'utilisateur à la section des photographes
+    photographersSection.appendChild(userCardDOM);
+  });
+}
+
+// Cette fonction d'initialisation est le point d'entrée du script.
+async function init() {
+  // Récupère les données des photographes en utilisant la fonction getPhotographers
+  const { photographers } = await getPhotographers();
+
+  // Affiche les données récupérées sur la page
+  displayData(photographers);
+}
+
+// Appelle la fonction d'initialisation pour commencer l'exécution du script
+init();
--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,39 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+  const { name, portrait, city, country, tagline, price } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+  const picture = `assets/photographers/Photographers_ID_Photos/${portrait}`;
 
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
+  function getUserCardDOM() {
+    //Content
+    const article = document.createElement("article");
+    const link = document.createElement("a");
+    const container = document.createElement("p");
+    //Image
+    const img = document.createElement("img");
+    img.setAttribute("src", picture);
+    img.setAttribute("alt", "photo de " + name);
+    //Name
+    const h2 = document.createElement("h2");
+    h2.textContent = name;
+    //Place
+    const localisation = document.createElement("span");
+    localisation.textContent = city + ", " + country;
+    //Bio
+    const bio = document.createElement("span");
+    bio.textContent = tagline;
+    //Price
+    const tarif = document.createElement("span");
+    tarif.textContent = price + "€/jour";
+
+    link.appendChild(img);
+    link.appendChild(h2);
+    container.appendChild(localisation);
+    container.appendChild(bio);
+    container.appendChild(tarif);
+    article.appendChild(link);
+    article.appendChild(container);
+
+    return article;
+  }
+  return { name, picture, getUserCardDOM };
 }
